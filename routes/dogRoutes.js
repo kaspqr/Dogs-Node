aa1c1234dba7d30d7e938de5749c0a478cafe66f
kaspqr <kasparvella@gmail.com>
2023-06-15 14:54:56 +0300

    authentication

--- routes/dogRoutes.js
+++ routes/dogRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const dogsController = require('../controllers/dogsController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(dogsController.getAllDogs)
