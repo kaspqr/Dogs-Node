ea344c000fa6f3f2d838725ecbdb01002ad568f8
kaspqr <kasparvella@gmail.com>
2023-06-16 14:39:57 +0300

    jwt auth completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,7 +32,7 @@ const login = asyncHandler(async (req, res) => {
             }
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '10m' }
+        { expiresIn: '15m' }
     )
 
     const refreshToken = jwt.sign(
@@ -83,7 +83,7 @@ const refresh = (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '10m' }
+                { expiresIn: '15m' }
             )
 
             res.json({ accessToken })
