b8d89aa6669c50d3931d10d5fea6d28e524b36eb
kaspqr <kasparvella@gmail.com>
2023-08-27 14:14:01 +0300

    changed allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.mydognotesapp.com',
-    'https://mydognotesapp.com',
+    'https://www.pawretriever.com',
+    'https://pawretriever.com',
 ]
 
 module.exports = allowedOrigins
