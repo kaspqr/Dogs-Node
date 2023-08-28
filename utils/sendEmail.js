fa61d7f52571256a3dc1e5039a677cd5468751f2
kaspqr <kasparvella@gmail.com>
2023-08-28 13:19:55 +0300

    new api key for mailgun

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,8 +5,8 @@ const mg = mailgun.client({ username: 'api', key: process.env.MAIL_API_KEY })
 
 const sendEmail = (email, subject, html) => {
 
-    mg.messages.create('sandboxbcd5a836fe9e486fb34b1b6263e6e542.mailgun.org', {
-        from: "Paw Retriever <mailgun@sandboxbcd5a836fe9e486fb34b1b6263e6e542.mailgun.org>",
+    mg.messages.create(process.env.MAIL_API_DOMAIN, {
+        from: `Paw Retriever <noreply@${MAIL_API_DOMAIN}>`,
         to: [`${email}`],
         subject: `${subject}`,
         html: `${html}`,
