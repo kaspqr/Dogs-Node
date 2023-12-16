0084f7263ac964da3b4cffc9e04c94b907966f85
kaspqr <kasparvella@gmail.com>
2023-12-16 22:50:00 +0200

    removed localhost from allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'exp://exp.host/@kaspqr/pawretriever?release-channel=default',
-    process.env.BASE_URL,
     'https://pawretriever.onrender.com',
     'https://www.pawretriever.com',
     'https://pawretriever.com',
